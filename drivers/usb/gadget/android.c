5134f48c9ab266c611cd685f3eef9406803b5751
bubor <bubor@dualhoki.vim.hu>
2013-11-04 21:31:51 +0100

    usb_storage mode fix, usb serial console usb storage showed only one sd card

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -627,79 +627,77 @@ struct mass_storage_function_config {
 	struct fsg_common *common;
 };
 
+#if defined(CONFIG_MACH_N1) || defined(CONFIG_MACH_BOSE_ATT)
+# define UMS_WITH_MULTIPLE_LUNS
+#endif
+
 static int mass_storage_function_init(struct android_usb_function *f,
 					struct usb_composite_dev *cdev)
 {
 	struct mass_storage_function_config *config;
 	struct fsg_common *common;
 	int err;
-#ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
+#ifdef UMS_WITH_MULTIPLE_LUNS
 	int i;
 #endif
-	config = kzalloc(sizeof(struct mass_storage_function_config),
-								GFP_KERNEL);
+
+	config = kzalloc(sizeof(struct mass_storage_function_config), GFP_KERNEL);
 	if (!config)
 		return -ENOMEM;
 
-#ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
-	if (config) {
-		/* All of Nvidia device have SD card slot so we make 2 luns */
-		config->fsg.nluns = 2;
-		printk(KERN_DEBUG "usb: %s, nluns=%d\n", __func__,
-				config->fsg.nluns);
+#ifdef UMS_WITH_MULTIPLE_LUNS
+	/* All of Nvidia device have SD card slot so we make 2 luns */
+	config->fsg.nluns = 2;
+	pr_notice("usb: %s, nluns=%d\n", __func__,
+			config->fsg.nluns);
 
-		for (i = 0; i < config->fsg.nluns; i++) {
-			config->fsg.luns[i].removable = 1;
-			config->fsg.luns[i].nofua = 1;
-		}
+	for (i = 0; i < config->fsg.nluns; i++) {
+		config->fsg.luns[i].removable = 1;
+		config->fsg.luns[i].nofua = 1;
+	}
 
-		common = fsg_common_init(NULL, cdev, &config->fsg);
-		if (IS_ERR(common)) {
+	common = fsg_common_init(NULL, cdev, &config->fsg);
+	if (IS_ERR(common)) {
+		kfree(config);
+		return PTR_ERR(common);
+	}
+
+	for (i = 0; i < config->fsg.nluns; i++) {
+		char luns[5];
+		err = snprintf(luns, 5, "lun%d", i);
+		if (err == 0) {
+			printk(KERN_ERR "usb: %s snprintf error\n",
+					__func__);
 			kfree(config);
-			return PTR_ERR(common);
+			return err;
 		}
-
-		for (i = 0; i < config->fsg.nluns; i++) {
-			char luns[5];
-			err = snprintf(luns, 5, "lun%d", i);
-			if (err == 0) {
-				printk(KERN_ERR "usb: %s snprintf error\n",
-						__func__);
-				kfree(config);
-				return err;
-			}
-			err = sysfs_create_link(&f->dev->kobj,
-					&common->luns[i].dev.kobj,
-					luns);
-			if (err) {
-				kfree(config);
-				return err;
-			}
-		}
-	} else {
-#endif
-		/* original mainline code */
-		printk(KERN_DEBUG "usb: %s pdata is not available. nluns=1\n",
-				__func__);
-		config->fsg.nluns = 1;
-		config->fsg.luns[0].removable = 1;
-
-		common = fsg_common_init(NULL, cdev, &config->fsg);
-		if (IS_ERR(common)) {
-			kfree(config);
-			return PTR_ERR(common);
-		}
-
 		err = sysfs_create_link(&f->dev->kobj,
-				&common->luns[0].dev.kobj,
-				"lun");
+				&common->luns[i].dev.kobj,
+				luns);
 		if (err) {
 			kfree(config);
 			return err;
 		}
-#ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
+	}
+#else
+	config->fsg.nluns = 1;
+	config->fsg.luns[0].removable = 1;
+
+	common = fsg_common_init(NULL, cdev, &config->fsg);
+	if (IS_ERR(common)) {
+		kfree(config);
+		return PTR_ERR(common);
+	}
+
+	err = sysfs_create_link(&f->dev->kobj,
+			&common->luns[0].dev.kobj,
+			"lun");
+	if (err) {
+		kfree(config);
+		return err;
 	}
 #endif
+
 	config->common = common;
 	f->config = config;
 	return 0;
@@ -1098,7 +1096,6 @@ static ssize_t state_show(struct device *pdev, struct device_attribute *attr,
 		state = "CONNECTED";
 	spin_unlock_irqrestore(&cdev->lock, flags);
 out:
-	printk(KERN_DEBUG "usb: %s buf=%s\n", __func__, state);
 	return sprintf(buf, "%s\n", state);
 }
 
@@ -1134,10 +1131,7 @@ field ## _store(struct device *dev, struct device_attribute *attr,	\
 		const char *buf, size_t size)				\
 {									\
 	if (size >= sizeof(buffer)) return -EINVAL;			\
-	if (sscanf(buf, "%s", buffer) == 1) {				\
-		return size;						\
-	}								\
-	return -1;							\
+	return strlcpy(buffer, buf, sizeof(buffer));			\
 }									\
 static DEVICE_ATTR(field, S_IRUGO | S_IWUSR, field ## _show, field ## _store);
 
